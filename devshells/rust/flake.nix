42ccfc37854e4d8858d163daf099b8bb0916500c
Cars Chandler <carschandler7@gmail.com>
2024-05-13 06:23:10 -0500

    Remove flakeutils from rust devshell

--- devshells/rust/flake.nix
+++ devshells/rust/flake.nix
@@ -8,39 +8,34 @@
       url = "github:oxalica/rust-overlay";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
       };
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+  outputs = { nixpkgs, rust-overlay, ... }:
+    let
+      system = "x86_64-linux";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
 
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-          # targets = [];
-        };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            rustToolchain
-            (rust-analyzer.override {
-              rustSrc = "${rustToolchain}/lib/rustlib/src/rust/library";
-            })
-          ];
-
-          shellHook = ''
-            
-          '';
-        };
-      }
-    );
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" ];
+        # targets = [];
+      };
+    in
+    with pkgs;
+    {
+      devShells.default = mkShell {
+        buildInputs = [
+          rustToolchain
+          (rust-analyzer.override {
+            rustSrc = "${rustToolchain}/lib/rustlib/src/rust/library";
+          })
+        ];
+        packages = [];
+      };
+    };
 }
 
