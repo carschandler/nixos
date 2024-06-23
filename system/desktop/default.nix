73a8d38ff3c0d76b85340a793207d9a50641f6ce
carschandler <92899389+carschandler@users.noreply.github.com>
2024-06-23 13:57:35 -0500

    Remove desktop WM config since in personal now

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -18,23 +18,6 @@
   boot.loader.grub.gfxmodeEfi = "1280x1024";
   boot.loader.grub.font = "${pkgs.source-code-pro}/share/fonts/opentype/SourceCodePro-Medium.otf";
 
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = [
-        pkgs.dmenu #application launcher most people use
-        pkgs.i3status # gives you the default i3 status bar
-        pkgs.i3lock #default i3 screen locker
-        pkgs.i3blocks #if you are planning on using i3blocks over i3status
-     ];
-    };
-    windowManager.awesome = {
-      enable = true;
-    };
-  };
-
   hardware.nvidia = {
     # This fixed hyprland crashing after waking from suspend!
     powerManagement.enable = true;
