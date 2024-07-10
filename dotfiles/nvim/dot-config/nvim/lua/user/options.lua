0ad1d803da9223fd4e2420ed85cc2f29dc8750e1
carschandler <92899389+carschandler@users.noreply.github.com>
2024-07-10 11:20:43 -0500

    fidget.lua for vim notifications

--- dotfiles/nvim/dot-config/nvim/lua/user/options.lua
+++ dotfiles/nvim/dot-config/nvim/lua/user/options.lua
@@ -20,7 +20,7 @@ set ttimeoutlen=5
 set textwidth=80
 set formatoptions=cqjnr
 " Add bulleted lists
-set formatlistpat=^\\s*[\\d-]\\+[\\]:.)}\\t\ ]\\s*
+set formatlistpat=^\\s*\\%(\\d\\|[-*+]\\)\\+[\\]:.)}\\t\ ]\\s*
 set number
 set linebreak
 ]])
@@ -34,3 +34,7 @@ vim.filetype.add({
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldmethod = "expr"
 vim.o.foldlevelstart = 99
+
+-- TODO rounded statuslines? Need an autocmd so that it updates each time a
+-- window is split/moved
+-- :setlocal statusline=%1*%*%<%f\ %h%m%r%=%-14.(%l,%c%)\ %P%1*
---
+++ dotfiles/nvim/dot-config/nvim/lua/user/plugins/fidget.lua
@@ -0,0 +1,13 @@
+return {
+  "j-hui/fidget.nvim",
+  opts = {
+    notification = {
+      override_vim_notify = true,
+      view = {},
+      window = {
+        -- normal_hl = "",
+        winblend = 0,
+      },
+    },
+  },
+}
