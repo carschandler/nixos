40b4d19268e03134b79b95b1cd1303be537dd536
carschandler <92899389+carschandler@users.noreply.github.com>
2024-07-29 14:25:21 -0500

    Mess with pyright organize imports for conform.nvim

--- dotfiles/nvim/dot-config/nvim/lua/user/plugins/conform.lua
+++ dotfiles/nvim/dot-config/nvim/lua/user/plugins/conform.lua
@@ -1,28 +1,67 @@
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   callback = function(args)
+--     if vim.bo[args.buf].filetype == "python" then
+--       vim.cmd.bufdo("PyrightOrganizeImports")
+--     end
+--   end,
+-- })
+
 return {
   "stevearc/conform.nvim",
   config = function()
     local conform = require("conform")
+
+    -- local function pyright_organize_imports(bufnr)
+    --   local params = {
+    --     command = "pyright.organizeimports",
+    --     arguments = { vim.uri_from_bufnr(bufnr) },
+    --   }
+    --
+    --   local clients = require("lspconfig.util").get_lsp_clients({
+    --     bufnr = bufnr,
+    --     name = "pyright",
+    --   })
+    --   for _, client in ipairs(clients) do
+    --     client.request("workspace/executeCommand", params, nil, 0)
+    --   end
+    -- end
+    --
     conform.setup({
       formatters_by_ft = {
-        python = function()
-          if vim.api.nvim_get_commands({})["PyrightOrganizeImports"] ~= nil then
-            vim.cmd("PyrightOrganizeImports")
-          end
-          return { "black" }
-        end,
+        -- python = function()
+        --   pcall(pyright_organize_imports)
+        --   -- pcall(nf)
+        --   return { "black" }
+        -- end,
+        python = { "isort", "black" },
         rust = { "rustfmt" },
         lua = { "stylua" },
       },
 
-      formatters = {
-        pyright_organize_imports = {},
-      },
+      -- -- This doesn't work properly
+      -- formatters = {
+      --   pyright_organize_imports = {
+      --     command = function()
+      --       if vim.api.nvim_get_commands({})["PyrightOrganizeImports"] ~= nil then
+      --         vim.cmd("PyrightOrganizeImports")
+      --       end
+      --       return "true"
+      --     end,
+      --   },
+      -- },
 
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
+        if vim.bo[bufnr].filetype == "python" then
+          pyright_organize_imports(bufnr)
+        end
+
+        return { timeout_ms = 500, lsp_format = "fallback", async = false }
+      end,
     })
 
     conform.formatters.black = {
@@ -32,16 +71,34 @@ return {
       },
     }
 
-    vim.keymap.set("n", "<Leader>lf", function()
-      conform.format()
-      if vim.o.filetype == "python" then
-        vim.cmd("PyrightOrganizeImports")
-        print(" ")
-      end
-    end, { desc = "Format buffer" })
+    vim.keymap.set("n", "<Leader>lf", conform.format, { desc = "Format buffer" })
+    vim.keymap.set("n", "<M-F>", conform.format, { desc = "Format buffer" })
 
-    vim.keymap.set("n", "<M-F>", function()
-      conform.format()
-    end, { desc = "Format buffer" })
+    -- Set commands to disable format-on-save temporarily
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
+    vim.api.nvim_create_user_command("W", function()
+      vim.b.disable_autoformat = true
+      vim.cmd.write()
+    end, {
+      desc = "Write without formatting",
+    })
   end,
 }
