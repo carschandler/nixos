33fa7651ec6e8c724be129dd87c8b386933046ca
carschandler <92899389+carschandler@users.noreply.github.com>
2025-03-17 22:16:15 -0500

    Typo

--- system/homelab/hardware/virtualization.nix
+++ system/homelab/hardware/virtualization.nix
@@ -1,5 +1,5 @@
 { lib, config, ... }:
 {
-  virtualization.incus.enable = true;
+  virtualisation.incus.enable = true;
   networking.nftables.enable = lib.mkIf config.virtualisation.incus.enable true;
 }
