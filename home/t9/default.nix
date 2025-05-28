fd2cb0ce6820376edd690830ee814d93c8f4421e
carschandler <92899389+carschandler@users.noreply.github.com>
2025-05-28 15:36:48 -0500

    Add terraform to t9

--- home/t9/default.nix
+++ home/t9/default.nix
@@ -7,6 +7,7 @@
     # pkgs.thunderbird
     pkgs.awscli2
     pkgs.aws-sam-cli
+    pkgs.terraform
     (
       if (lib.versionAtLeast pkgs.livekit-cli.version "2.4.1") then
         (lib.warn "pkgs.livekit-cli has updated to 2.4.1; remove manual build" pkgs.livekit-cli)
