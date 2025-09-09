977419af090ae2cab0cf00557b01f75369006c5b
carschandler <92899389+carschandler@users.noreply.github.com>
2025-09-09 07:57:48 -0500

    Add nix settings to homelab system config

--- system/homelab/default.nix
+++ system/homelab/default.nix
@@ -86,4 +86,21 @@
   };
 
   system.stateVersion = "24.05";
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
