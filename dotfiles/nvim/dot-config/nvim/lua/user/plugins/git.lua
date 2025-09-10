af5b3bfdc5efdaa8efcad8a00a6e3bcdb573d52c
carschandler <92899389+carschandler@users.noreply.github.com>
2025-09-10 12:02:29 -0500

    Gitsigns keybinds

--- dotfiles/nvim/dot-config/nvim/lua/user/plugins/git.lua
+++ dotfiles/nvim/dot-config/nvim/lua/user/plugins/git.lua
@@ -1,60 +1,60 @@
 return {
-	{ "tpope/vim-fugitive" },
-	{
-		"NeogitOrg/neogit",
-		cond = not vim.g.vscode,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			"ibhagwan/fzf-lua",
-		},
-		keys = {
-			{
-				"<Leader>g",
-				function()
-					require("neogit").open()
-				end,
-				desc = "Git",
-			},
-		},
-		opts = {
-			signs = {
-				-- item = { "", "" },
-				-- section = { "", "" },
-				-- item = { "", "" },
-				-- section = { "", "" },
-				-- item = { "󰍟", "󰍝" },
-				-- section = { "󰍟", "󰍝" },
-				-- item = { "", "" },
-				-- section = { "", "" },
-				item = { "", "" },
-				section = { "", "" },
-			},
-			status = {
-				mode_padding = 1,
-				mode_text = {
-					M = "󰷉",
-					N = "󰻭",
-					A = "󰸩",
-					D = "󱀷",
-					C = "󰬳",
-					U = "󱈗",
-					R = "󱀱",
-					["?"] = "󱀶",
-					DD = "󰩌",
-					DU = "󰩌",
-					UD = "󰩌",
-					AA = "󰩌",
-					AU = "󰩌",
-					UA = "󰩌",
-					UU = "󰩌",
-				},
-			},
-			mappings = {
-				status = {
-					["="] = "Toggle",
-				},
-			},
-		},
-	},
+  { "tpope/vim-fugitive" },
+  {
+    "NeogitOrg/neogit",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    keys = {
+      {
+        "<Leader>gg",
+        function()
+          require("neogit").open()
+        end,
+        desc = "Git",
+      },
+    },
+    opts = {
+      signs = {
+        -- item = { "", "" },
+        -- section = { "", "" },
+        -- item = { "", "" },
+        -- section = { "", "" },
+        -- item = { "󰍟", "󰍝" },
+        -- section = { "󰍟", "󰍝" },
+        -- item = { "", "" },
+        -- section = { "", "" },
+        item = { "", "" },
+        section = { "", "" },
+      },
+      status = {
+        mode_padding = 1,
+        mode_text = {
+          M = "󰷉",
+          N = "󰻭",
+          A = "󰸩",
+          D = "󱀷",
+          C = "󰬳",
+          U = "󱈗",
+          R = "󱀱",
+          ["?"] = "󱀶",
+          DD = "󰩌",
+          DU = "󰩌",
+          UD = "󰩌",
+          AA = "󰩌",
+          AU = "󰩌",
+          UA = "󰩌",
+          UU = "󰩌",
+        },
+      },
+      mappings = {
+        status = {
+          ["="] = "Toggle",
+        },
+      },
+    },
+  },
 }
--- dotfiles/nvim/dot-config/nvim/lua/user/plugins/gitsigns.lua
+++ dotfiles/nvim/dot-config/nvim/lua/user/plugins/gitsigns.lua
@@ -1,5 +1,34 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	cond = not vim.g.vscode,
-	config = true,
+  "lewis6991/gitsigns.nvim",
+  cond = not vim.g.vscode,
+  opts = {
+    on_attach = function(_bufnumber)
+      if pcall(require, "which-key") then
+        require("which-key").add({
+          { "<Leader>g", group = "git" },
+        })
+      end
+      vim.keymap.set("n", "]g", function()
+        require("gitsigns").nav_hunk("next")
+      end, { desc = "next hunk" })
+      vim.keymap.set("n", "[g", function()
+        require("gitsigns").nav_hunk("prev")
+      end, { desc = "previous hunk" })
+      vim.keymap.set("n", ";gp", function()
+        require("gitsigns").preview_hunk_inline()
+      end, { desc = "preview hunk" })
+      vim.keymap.set("n", ";gP", function()
+        require("gitsigns").preview_hunk()
+      end, { desc = "preview hunk (windowed)" })
+      vim.keymap.set("n", ";gr", function()
+        require("gitsigns").reset_hunk()
+      end, { desc = "reset hunk" })
+      vim.keymap.set("n", ";gs", function()
+        require("gitsigns").stage_hunk()
+      end, { desc = "stage hunk" })
+      vim.keymap.set("n", ";gb", function()
+        require("gitsigns").blame()
+      end, { desc = "blame" })
+    end,
+  },
 }
