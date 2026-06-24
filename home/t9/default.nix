733b5084ea2c146127fc70b84c0b4aea949b9a0c
carschandler <92899389+carschandler@users.noreply.github.com>
2026-06-24 11:12:37 -0500

    fix: only include vite for linux systems

--- home/t9/default.nix
+++ home/t9/default.nix
@@ -16,26 +16,27 @@
   #   fi
   # '';
 
-  home.packages = [
-    inputs.conview.packages.${pkgs.stdenv.hostPlatform.system}.default
+  home.packages = (
+    [
+      inputs.conview.packages.${pkgs.stdenv.hostPlatform.system}.default
 
-    # (pkgs.callPackage ./awscli2/package.nix { })
-    pkgs.act
-    pkgs.awscli2
-    pkgs.ssm-session-manager-plugin
-    # pkgs.aws-sam-cli
-    pkgs.terraform
-    pkgs.nodejs_22
-    pkgs.ngrok
-    pkgs.pnpm
-    pkgs.typescript-language-server
-    pkgs.twilio-cli
-    pkgs.crush
-    pkgs.ffmpeg
-    pkgs.vite
-    # pkgs.livekit-cli
-    (pkgs.callPackage ./livekit-cli/package.nix { })
-  ];
+      # (pkgs.callPackage ./awscli2/package.nix { })
+      pkgs.act
+      pkgs.awscli2
+      pkgs.ssm-session-manager-plugin
+      # pkgs.aws-sam-cli
+      pkgs.terraform
+      pkgs.ngrok
+      pkgs.nodejs_24
+      pkgs.pnpm
+      pkgs.typescript-language-server
+      pkgs.twilio-cli
+      pkgs.crush
+      pkgs.ffmpeg
+      pkgs.livekit-cli
+    ]
+    ++ (if lib.strings.hasSuffix "linux" pkgs.stdenv.hostPlatform.system then [ pkgs.vite ] else [ ])
+  );
 
   programs = {
     # claude-code = {
